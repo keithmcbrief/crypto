f9197859d39acfe2b8bc32ecaa9c1bab4c93c5f9
keithmcbrief <yurij97@gmail.com>
2022-07-30 01:23:22 -0500

    c

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Coin from "./pages/Coin";
+import Coin from "./pages/CoinPage";
 import Home from "./pages/Home";
 import React from 'react'
 
