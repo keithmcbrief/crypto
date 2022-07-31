58560713eb8cf7d4487f7dbcbb70056635d7c7d8
keithmcbrief <yurij97@gmail.com>
2022-07-30 21:56:21 -0500

    menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import ThemeToggle from "./ThemeToggle";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <div
       className="rounded-div flex items-center 
@@ -12,33 +14,60 @@ const Navbar = () => {
       <Link to="/">
         <h1 className="text-2xl">Cryptocoin</h1>
       </Link>
-      <div className="hidden md:flex">
+      <div className="hidden md:block">
         <ThemeToggle />
       </div>
-      <div>
-        <Link to="signin">Sign In</Link>
-        <Link to="signup">Sign Up</Link>
+      <div className="hidden md:block">
+        <Link className="p-4 hover:text-accent" to="signin">
+          Sign In
+        </Link>
+        <Link
+          className="bg-button text-btnText px-5 py-2 ml-2 
+          rounded-2xl shadow-lg hover:shadow-2xl"
+          to="signup"
+        >
+          Sign Up
+        </Link>
       </div>
-      <div>
-        <AiOutlineMenu />
+      <div
+        onClick={() => setShowMenu(!showMenu)}
+        className="block md:hidden text-2xl cursor-pointer z-10 "
+      >
+        {!showMenu ? <AiOutlineMenu /> : <AiOutlineClose />}
       </div>
       {/* Menu */}
-      <div>
-        <ul>
-          <li>
+      <div
+        className={
+          showMenu
+            ? "md:hidden fixed left-0 top-20 flex flex-col items-center justify-between w-full h-[90%] bg-primary ease-in duration-200 z-100"
+            : "fixed left-[-100%] top-20 h-[90%] flex flex-col items-center justify-between ease-in duration-200"
+        }
+      >
+        <ul className="w-full p-4">
+          <li className="border-b py-6 flex justify-center">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="border-b py-6 flex justify-center">
             <Link to="/">Account</Link>
           </li>
-          <li>
+          <li className="py-6 flex justify-center">
             <ThemeToggle />
           </li>
         </ul>
-        <div>
+        <div className="flex flex-col w-full p-4">
           <Link to="/signin">
-            <button>Sign In</button>
-            <button>Sign Up</button>
+            <button
+              className="w-full my-2 p-3 bg-primary
+            border border-secondary rounded-2xl shadow-xl"
+            >
+              Sign In
+            </button>
+            <button
+              className="w-full my-2 p-3 bg-button
+            text-btnText rounded-2xl shadow-xl"
+            >
+              Sign Up
+            </button>
           </Link>
         </div>
       </div>
