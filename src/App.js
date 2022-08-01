7754bbafd6aec404f56d41651c2438d0e836d971
keithmcbrief <yurij97@gmail.com>
2022-07-31 20:47:41 -0500

    api

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Account from "./pages/Account";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ThemeProvider } from "./context/ThemeContext";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
+import axios from "axios";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true";
+
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setCoins(res.data);
+      console.log(res.data);
+    });
+  }, [url]);
+
   return (
     <ThemeProvider>
       <Navbar />
