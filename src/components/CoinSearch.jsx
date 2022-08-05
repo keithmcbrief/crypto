efdbd267f49d189a27ba35f7931fb6e0e151dd85
keithmcbrief <yurij97@gmail.com>
2022-08-04 23:00:57 -0500

    c

--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import { AiOutlineStar } from 'react-icons/ai'
+import React from "react";
+import { AiOutlineStar } from "react-icons/ai";
+import { Sparklines, SparklinesLine } from "react-sparklines";
 
 const CoinSearch = ({ coins }) => {
+  console.log(coins[0].sparkline_in_7d.price);
   return (
     <div>
       <div>
         <h1>Coins</h1>
         <form>
-          <input type="text" placeholder='Search For A Coin' />
+          <input type="text" placeholder="Search For A Coin" />
         </form>
       </div>
 
@@ -25,23 +27,34 @@ const CoinSearch = ({ coins }) => {
           </tr>
         </thead>
         <tbody>
-          {coins.map(coin => (
+          {coins.map((coin) => (
             <tr>
-              <td><AiOutlineStar /></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+              <td>
+                <AiOutlineStar />
+              </td>
+              <td>{coin.market_cap_rank}</td>
+              <td>
+                <div>
+                  <img src={coin.image} alt={coin.id} />
+                  <p>{coin.name}</p>
+                </div>
+              </td>
+              <td>{coin.symbol}</td>
+              <td>{coin.current_price}</td>
+              <td>{coin.price_change_percentage_24h}</td>
+              <td>{coin.total_volume}</td>
+              <td>{coin.market_cap}</td>
+              <td>
+                <Sparklines data={coin.sparkline_in_7d.price}>
+                  <SparklinesLine color="teal" />
+                </Sparklines>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default CoinSearch
+export default CoinSearch;
