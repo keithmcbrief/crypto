698fcad8d3f9a0adcbf075aa04c35ed23a9b3dbf
keithmcbrief <yurij97@gmail.com>
2022-08-04 23:46:50 -0500

    c

--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -9,7 +9,7 @@ const CoinSearch = ({ coins }) => {
     <div className="rounded-div my-4">
       <div className="flex flex-col md:flex-row
       justify-between pt-2 pb-6 text-center md:text-right">
-        <h1>Coins</h1>
+        <h1 className="text-2xl font-bold my-2">Coins</h1>
         <form>
           <input
             value={searchInput}
