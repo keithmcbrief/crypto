181c35b55265be44a429d2dd0b84a38a742f3279
keithmcbrief <yurij97@gmail.com>
2022-08-04 23:29:20 -0500

    fixed graphs

---
+++ src/components/CoinItem.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { AiOutlineStar } from "react-icons/ai";
+import { Sparklines, SparklinesLine } from "react-sparklines";
+
+const CoinItem = ({ coin }) => {
+  return (
+    <div>
+      <tr>
+        <td>
+          <AiOutlineStar />
+        </td>
+        <td>{coin.market_cap_rank}</td>
+        <td>
+          <div>
+            <img src={coin.image} alt={coin.id} />
+            <p>{coin.name}</p>
+          </div>
+        </td>
+        <td>{coin.symbol}</td>
+        <td>{coin.current_price}</td>
+        <td>{coin.price_change_percentage_24h}</td>
+        <td>{coin.total_volume}</td>
+        <td>{coin.market_cap}</td>
+        <td className='w-20'>
+          <Sparklines data={coin.sparkline_in_7d.price}>
+            <SparklinesLine color="teal" />
+          </Sparklines>
+        </td>
+      </tr>
+    </div>
+  );
+};
+
+export default CoinItem;
--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { AiOutlineStar } from "react-icons/ai";
-import { Sparklines, SparklinesLine } from "react-sparklines";
+
+import CoinItem from "./CoinItem";
 
 const CoinSearch = ({ coins }) => {
-  console.log(coins[0].sparkline_in_7d.price);
   return (
     <div>
       <div>
@@ -28,28 +27,7 @@ const CoinSearch = ({ coins }) => {
         </thead>
         <tbody>
           {coins.map((coin) => (
-            <tr>
-              <td>
-                <AiOutlineStar />
-              </td>
-              <td>{coin.market_cap_rank}</td>
-              <td>
-                <div>
-                  <img src={coin.image} alt={coin.id} />
-                  <p>{coin.name}</p>
-                </div>
-              </td>
-              <td>{coin.symbol}</td>
-              <td>{coin.current_price}</td>
-              <td>{coin.price_change_percentage_24h}</td>
-              <td>{coin.total_volume}</td>
-              <td>{coin.market_cap}</td>
-              <td>
-                <Sparklines data={coin.sparkline_in_7d.price}>
-                  <SparklinesLine color="teal" />
-                </Sparklines>
-              </td>
-            </tr>
+            <CoinItem key={coin.id} coin={coin} />
           ))}
         </tbody>
       </table>
