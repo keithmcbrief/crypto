4f34363ed9036ddd5a9492cd0bee4fc84367e140
keithmcbrief <yurij97@gmail.com>
2022-08-04 22:31:44 -0500

    commit

--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,14 +1,45 @@
 import React from 'react'
+import { AiOutlineStar } from 'react-icons/ai'
 
 const CoinSearch = ({ coins }) => {
   return (
     <div>
       <div>
-        <h1>Search Coins</h1>
+        <h1>Coins</h1>
         <form>
           <input type="text" placeholder='Search For A Coin' />
         </form>
       </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>#</th>
+            <th>Coin</th>
+            <th></th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>Mkt</th>
+            <th>Last 7 Days</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map(coin => (
+            <tr>
+              <td><AiOutlineStar /></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
