ed8288c030c5eeeee96f0f0727dea0d513f392b7
keithmcbrief <yurij97@gmail.com>
2022-08-04 23:51:01 -0500

    search styling

--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -7,13 +7,17 @@ const CoinSearch = ({ coins }) => {
 
   return (
     <div className="rounded-div my-4">
-      <div className="flex flex-col md:flex-row
-      justify-between pt-2 pb-6 text-center md:text-right">
+      <div
+        className="flex flex-col md:flex-row
+      justify-between pt-2 pb-6 text-center md:text-right"
+      >
         <h1 className="text-2xl font-bold my-2">Coins</h1>
         <form>
           <input
-            value={searchInput}
             onChange={(event) => setSearchInput(event.target.value)}
+            className="w-full bg-primary border border-input
+            px-4 py-2 rounded-2xl shadow-xl"
+            value={searchInput}
             type="text"
             placeholder="Search For A Coin"
           />
