9acb8d59e0468e7c3d9b53ed7a2d43ed092b2841
keithmcbrief <yurij97@gmail.com>
2022-08-06 15:55:14 -0500

    c

---
+++ src/components/Trending.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Trending = () => {
+    const [] = useState([])
+    const url = 'https://api.coingecko.com/api/v3/search/trending';
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Trending
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CoinSearch from "../components/CoinSearch";
+import Trending from "../components/Trending";
 
 export default function Home({ coins }) {
   return (
     <div>
       <CoinSearch coins={coins} />
+      <Trending />
     </div>
   );
 }
