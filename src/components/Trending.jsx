3022eda8393b7258069de5a809cac9f154bbd37f
keithmcbrief <yurij97@gmail.com>
2022-08-06 15:57:41 -0500

    commit

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -2,8 +2,16 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Trending = () => {
-    const [] = useState([])
+    const [trendingCoins, setTrendingCoins] = useState([])
     const url = 'https://api.coingecko.com/api/v3/search/trending';
+
+    useEffect(() => {
+        axios.get(url)
+        .then((res) => {
+            console.log(res)
+            setTrendingCoins(res.data)
+        })
+    }, [])
 
   return (
     <div>
